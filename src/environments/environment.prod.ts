145108e20465eefc5380f6d8af49c98155049bb8
unknown <arun10072000@gmail.com>
2020-01-05 12:28:20 +0530

    Initial Production Build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,11 @@ export const environment = {
 
   production: true,
   backend: {
-    server: 'http://www.gyanmitra20.mepcoeng.ac.in'
+    //server: 'http://www.gyanmitra20.mepcoeng.ac.in'
+    server: 'http://172.16.16.151:3000'
   },
   frontend: {
-    server: 'http://www.gyanmitra20.mepcoeng.ac.in/'
+    server: 'http://172.16.16.151:3000/'
   },
   payment: {
     url: 'https://secure.payu.in/_payment',
